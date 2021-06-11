393f7485f489d7b68c7b4cdc468e0487b0bc32a8
gouravmarch20 <orangeorigion@gamil.com>
2021-06-11 21:38:54 +0530

    2. Aboutus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 
-function App() {
+import GlobalStyle from "./components/GlobalStyle";
+//Router
+import { Switch, Route, useLocation } from "react-router-dom";
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+      <Nav />
+      <Route path="/" exact>
+        <AboutUs />
+      </Route>
     </div>
   );
-}
+};
 
 export default App;
